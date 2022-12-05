930468d6e48cc57a7b9d32abece5fa94e060678e
zaiky09 <zamilmozamil@outlook.com>
2022-12-05 09:51:25 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+//SOLUTION
+
+var customerName = "bob";
+
+ function upperCaseCustomerName() {
+    customerName= 'bob'.toUpperCase();
+}
+
+
+function setBestCustomer()
+{
+    return bestCustomer = 'not bob';
+}
+function overwriteBestCustomer ()
+{
+    return bestCustomer = 'maybe bob';
+
+}
+const LeastFavoriteCustomer = 'you'
+function changeLeastFavoriteCustomer()
+{
+    return LeastFavoriteCustomer = 'Zamil'
+}
